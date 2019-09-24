e047cb36297d9aa3fba6f7f17237c16f6884c9d1
Aarsh Sharma <aarsh01rsh@gmail.com>
2019-09-25 00:12:53 +0530

    configs and minor changes
    Configs and keys added.
    Readme added.
    Follows removed from profile as it was not required.

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+const mongoURI = require("./credentials").mongoURI;
+const secretOrKey = require("./credentials").secretOrKey;
+
+module.exports = {
+  mongoURI,
+  secretOrKey
+};
--- server/server.js
+++ server/server.js
@@ -15,8 +15,6 @@ app.use(passport.initialize());
 
 // require('./config/passport')(passport);
 
-
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log("server is running!");
-});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
